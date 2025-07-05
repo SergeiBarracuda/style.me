c70cfb598cdc0d086f382a3d382f4ffd373c62b4
sergeibarracuda <sergeibarracuda@users.noreply.github.com>
2025-07-05 17:01:24 +0200

    chore: update package.json to include @types/next dependency

--- frontend/components/search-integration.test.tsx
+++ frontend/components/search-integration.test.tsx
@@ -1,10 +1,9 @@
 import type { ReactNode } from 'react';
 import { render, screen, fireEvent, waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom';
-import EnhancedSearchPage from '../../components/search/EnhancedSearchPage';
+import EnhancedSearchPage from '../search/EnhancedSearchPage';
 import * as SearchContextModule from '../../contexts/SearchContext';
 import * as mapsUtils from '../../lib/maps';
-
 /** Mock for next/image */
 jest.mock('next/image', () => ({
   __esModule: true,
